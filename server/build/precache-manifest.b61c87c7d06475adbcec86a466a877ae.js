308ecee39c4caefeea47dc0277984149e05f6d32
cruzador <batyrqh@protonmail.com>
2020-03-26 17:57:49 +0600

    added build

---
+++ server/build/precache-manifest.b61c87c7d06475adbcec86a466a877ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f4202930df0b2966ba059e66be2cac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b2798d53c3a5d8f39bd",
+    "url": "/static/css/main.7e06c26d.chunk.css"
+  },
+  {
+    "revision": "d1bd930cd824bea91ac8",
+    "url": "/static/js/2.0c2a74c0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.0c2a74c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b2798d53c3a5d8f39bd",
+    "url": "/static/js/main.9d65430b.chunk.js"
+  },
+  {
+    "revision": "8850848827b695a5b98b",
+    "url": "/static/js/runtime-main.65c244a8.js"
+  },
+  {
+    "revision": "b6bca56a0edc2124184a356e54a560aa",
+    "url": "/static/media/book1.b6bca56a.jpg"
+  },
+  {
+    "revision": "d4c4223e1944b854306397d324c4891d",
+    "url": "/static/media/book2.d4c4223e.jpg"
+  },
+  {
+    "revision": "cdb9b4acefe4aed15c0986c0ac63df83",
+    "url": "/static/media/book3.cdb9b4ac.jpg"
+  },
+  {
+    "revision": "e33ca057b2570114666b515e5c14f1b6",
+    "url": "/static/media/book4.e33ca057.jpg"
+  },
+  {
+    "revision": "512a5d94b268ab9c13d5b75eb3016dd7",
+    "url": "/static/media/book6.512a5d94.jpeg"
+  }
+]);
